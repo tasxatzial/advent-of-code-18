ed0e8ac65a68c4f0368ffa46bebafacbf33082c5
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-10-11 19:41:41 +0300

    day2: parse input

--- day02/src/day02/core.clj
+++ day02/src/day02/core.clj
@@ -1,7 +1,18 @@
 (ns day02.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse
+  "Splits the input string by \n and converts it into a vector of strings."
+  [s]
+  (clojure.string/split-lines s))
+
+(def box-ids (parse (slurp input-file)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println box-ids))

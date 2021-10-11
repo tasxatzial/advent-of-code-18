a388f7970fc92dfb3d99214c00391d05aeed9367
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-10-12 01:12:16 +0300

    day3: parse input

--- day03/src/day03/core.clj
+++ day03/src/day03/core.clj
@@ -1,7 +1,25 @@
 (ns day03.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse-line
+  "Parses a string representing a single claim."
+  [line]
+  (let [no-delims (clojure.string/split line #" @ |,|: |x")]
+    (mapv #(Integer/parseInt %) (rest no-delims))))
+
+(defn parse
+  "Splits the input string by \n and converts it to an appropriate data
+  structure."
+  [s]
+  (mapv parse-line (clojure.string/split-lines s)))
+
+(def claims (parse (slurp input-file)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println claims))

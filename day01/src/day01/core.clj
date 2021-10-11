cedf48517b6271eadbba29094fed1fbc90f0534f
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-10-11 15:46:56 +0300

    day1: parse input

--- day01/src/day01/core.clj
+++ day01/src/day01/core.clj
@@ -1,7 +1,20 @@
 (ns day01.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse
+  "Splits the input string by \n and converts it into a vector of numbers."
+  [s]
+  (->> s
+       clojure.string/split-lines
+       (mapv #(Integer/parseInt %))))
+
+(def frequency-changes (parse (slurp input-file)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println frequency-changes))

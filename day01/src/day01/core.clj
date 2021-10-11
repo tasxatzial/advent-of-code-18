28d7d13125cd49bc4d5f6df331a3b5421e2dc4e5
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-10-11 16:39:56 +0300

    day1 problem 2: compute solution

--- day01/src/day01/core.clj
+++ day01/src/day01/core.clj
@@ -15,6 +15,20 @@
 
 (def frequency-changes (parse (slurp input-file)))
 
+; --------------------------
+; problem 2
+
+(defn first-repeated-freq
+  "Finds the first frequency that is repeated twice."
+  ([] (first-repeated-freq #{0} 0 frequency-changes))
+  ([frequencies curr-frequency [frequency-change & rest-changes]]
+   (if frequency-change
+     (let [new-frequency (+ curr-frequency frequency-change)]
+       (if (contains? frequencies new-frequency)
+         new-frequency
+         (recur (conj frequencies new-frequency) new-frequency rest-changes)))
+     (recur frequencies curr-frequency frequency-changes))))
+
 ; ---------------------------------------
 ; results
 
@@ -22,6 +36,11 @@
   []
   (apply + frequency-changes))
 
+(defn day01-2
+  []
+  (first-repeated-freq))
+
 (defn -main
   []
-  (println (day01-1)))
+  (println (day01-1))
+  (println (day01-2)))

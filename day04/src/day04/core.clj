b6bf4abd8690eb5e5de6e16d48d639872c56f01f
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-10-12 21:43:10 +0300

    day4: preprocess a record string

--- day04/src/day04/core.clj
+++ day04/src/day04/core.clj
@@ -1,7 +1,24 @@
 (ns day04.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn preprocess-record
+  "Takes a record in string form, splits it into tokens and replaces
+  00 that appears in hour time with 24. Example:
+  \"[1518-10-05 00:02] Guard #3347 begins shift\"
+  becomes
+  [\"1518-10-05\" \"24:02\" \"Guard\" \"3347\" \"begins\" \"shift\"]"
+  [s]
+  (let [split-record (clojure.string/split s #"[#\[\] ]")
+        record-data (filterv #(not= "" %) split-record)
+        time (second record-data)
+        new-time (clojure.string/replace time #"00:" "24:")]
+    (assoc record-data 1 new-time)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println (preprocess-record "[1518-10-05 00:02] Guard #3347 begins shift")))

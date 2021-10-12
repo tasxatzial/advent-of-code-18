c0b9794997d54f5ba779c84802df21cbc9b8d4b6
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-10-12 21:43:53 +0300

    day4: postprocess a record

--- day04/src/day04/core.clj
+++ day04/src/day04/core.clj
@@ -19,6 +19,26 @@
         new-time (clojure.string/replace time #"00:" "24:")]
     (assoc record-data 1 new-time)))
 
+(defn postprocess-record
+  "Takes a record in the form returned by preprocess-record and
+  converts it to an appropriate data structure:
+  1) The date string remains as is.
+  2) The time string is replaced by a vector of [hour minute].
+  3) The rest is one of:
+     -1 (guard falls asleep)
+     -2 (guard awakes)
+     positive number (guard id)"
+  [[date time & rest-record]]
+  (let [[hour minute] (clojure.string/split time #":")
+        new-hour (Integer/parseInt hour)
+        new-minute (Integer/parseInt minute)]
+    (if (= 4 (count rest-record))
+      (let [guard-id (Integer/parseInt (second rest-record))]
+        [date [new-hour new-minute] guard-id])
+      (if (= "falls" (first rest-record))
+        [date [new-hour new-minute] -1]
+        [date [new-hour new-minute] -2]))))
+
 (defn -main
   []
-  (println (preprocess-record "[1518-10-05 00:02] Guard #3347 begins shift")))
+  (println (postprocess-record ["1518-10-05" "24:02" "Guard" "3347" "begins" "shift"])))

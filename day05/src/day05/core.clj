68490116dcf3dca69ee6a2ff315996d9f958faf6
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-10-14 03:10:43 +0300

    day5 problem 1: compute solution

--- day05/src/day05/core.clj
+++ day05/src/day05/core.clj
@@ -6,8 +6,32 @@
 
 (def input-file "resources\\input.txt")
 
-(def polymer (slurp input-file))
+(def polymer (butlast (slurp input-file)))
+
+; --------------------------
+; problem 1
+
+(defn react?
+  "Returns true if two letters can react, false otherwise."
+  [l1 l2]
+  (= 32 (Math/abs (- (int l1) (int l2)))))
+
+(defn react-polymer
+  "Return the polymer after all possible reactions."
+  []
+  (reverse (reduce (fn [result letter]
+            (if (and (seq result) (react? letter (first result)))
+              (rest result)
+              (conj result letter)))
+          (list (first polymer)) (rest polymer))))
+
+; --------------------------
+; results
+
+(defn day05-1
+  []
+  (count (react-polymer)))
 
 (defn -main
   []
-  (println polymer))
+  (println (day05-1)))

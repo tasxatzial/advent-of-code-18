900c8cc7577c62827689ac2275f70255460317a1
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-10-21 15:27:55 +0300

    day6: find the min and max in each of the x, y dimensions

--- day06/src/day06/core.clj
+++ day06/src/day06/core.clj
@@ -18,6 +18,40 @@
 
 (def coordinates (parse (slurp input-file)))
 
+(defn- find-edge
+  "Finds the {min,max} {x,y} coordinates.
+  fn-extreme must be 'min' or 'max'.
+  fn-coordinate must be 'first' (for the x coordinates) or 'second' (for the y coordinates)."
+  [fn-extreme fn-coordinate]
+  (->> coordinates
+       (mapv fn-coordinate)
+       (apply fn-extreme)))
+
+(defn find-max-x
+  "Finds the max x coordinate."
+  []
+  (find-edge max first))
+
+(defn find-min-x
+  "Finds the min x coordinate."
+  []
+  (find-edge min first))
+
+(defn find-max-y
+  "Finds the max y coordinate."
+  []
+  (find-edge max first))
+
+(defn find-min-y
+  "Finds the min y coordinate."
+  []
+  (find-edge min second))
+
+(def memoized-find-max-x (memoize find-max-x))
+(def memoized-find-min-x (memoize find-min-x))
+(def memoized-find-max-y (memoize find-max-y))
+(def memoized-find-min-y (memoize find-min-y))
+
 (defn -main
   []
-  (println coordinates))
+  (println (memoized-find-min-y)))

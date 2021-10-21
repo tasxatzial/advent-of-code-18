1411aceb1356c54774b304b149cd6ab9863ab056
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-10-21 14:59:55 +0300

    day6: parse input

--- day06/src/day06/core.clj
+++ day06/src/day06/core.clj
@@ -1,7 +1,23 @@
 (ns day06.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse
+  "Parses the input string and converts it to a vector of vectors.
+  Each vector represents a (x,y) coordinate."
+  [s]
+  (->> s
+       (clojure.string/split-lines)
+       (map #(clojure.string/split % #", "))
+       (mapv #(vector (Integer/parseInt (first %))
+                      (Integer/parseInt (second %))))))
+
+(def coordinates (parse (slurp input-file)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println coordinates))

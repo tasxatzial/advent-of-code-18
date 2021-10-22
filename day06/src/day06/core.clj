462c3cc386467a6508dbb2a64636e85c5b96266b
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-10-23 01:20:07 +0300

    day6 problem 1: find the nearest points to each of the input coordinates

--- day06/src/day06/core.clj
+++ day06/src/day06/core.clj
@@ -27,7 +27,7 @@
 ; --------------------------
 ; problem 1
 
-(defn- find-edge
+(defn- find-extreme-point
   "Finds the {min,max} {x,y} coordinates.
   fn-extreme must be 'min' or 'max'.
   fn-coordinate must be 'first' (for the x coordinates) or 'second' (for the y coordinates)."
@@ -36,19 +36,19 @@
        (mapv fn-coordinate)
        (apply fn-extreme)))
 
-(defn create-edge-points
+(defn find-extreme-points
   "Finds the min x, max x, min y, max y input coordinates and returns
   them in a map. They can be accessed with :xmin, :xmax, :ymin, :ymax keywords."
   []
-  {:xmin (find-edge min first)
-   :xmax (find-edge max first)
-   :ymin (find-edge min second)
-   :ymax (find-edge max second)})
+  {:xmin (find-extreme-point min first)
+   :xmax (find-extreme-point max first)
+   :ymin (find-extreme-point min second)
+   :ymax (find-extreme-point max second)})
 
-(def memoized-create-edge-points (memoize create-edge-points))
+(def memoized-create-edge-points (memoize find-extreme-points))
 
 (defn find-nearest-point
-  "Finds the nearest coordinate to (x,y) using the manhattan distance
+  "Finds the nearest input coordinate to (x,y) using the manhattan distance
   metric. Returns either the coordinate vector or -1 if there are more
   than 1 nearest coordinates."
   [[x y]]
@@ -65,6 +65,28 @@
           (recur rest-distances (inc index) nearest-coordinate-index))
         (get coordinates nearest-coordinate-index)))))
 
+(defn create-scan-grid
+  "Creates a vector of all coordinates which belong to a rectangle
+  area that includes all input coordinates."
+  []
+  (let [{:keys [xmin xmax ymin ymax]} (memoized-create-edge-points)]
+    (for [x (range xmin (inc xmax))
+          y (range ymin (inc ymax))]
+      [x y])))
+
+(defn find-nearest-points
+  "Find the nearest coordinates to each of the input coordinates.
+  Returns a map. Each keys is an input coordinate and the corresponding
+  value is a vector containing all nearest coordinates to the input
+  coordinate."
+  []
+  (reduce (fn [result coordinate]
+            (let [nearest-point (find-nearest-point coordinate)]
+              (if (= nearest-point -1)
+                result
+                (update result nearest-point #(conj % coordinate)))))
+          (zipmap coordinates (repeat [])) (create-scan-grid)))
+
 (defn -main
   []
-  (println (memoized-create-edge-points)))
+  (println (find-nearest-points)))

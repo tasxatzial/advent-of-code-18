187f31851ef8bc6ce523c9dcb36fe40723b557a4
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-10-28 23:33:57 +0300

    day7: parse input

--- day07/src/day07/core.clj
+++ day07/src/day07/core.clj
@@ -1,7 +1,22 @@
 (ns day07.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse
+  "Parses the input string and converts it to a vector of instructions.
+  Each instruction is a vector of two items that indicate the step letters
+  in each input line."
+  [s]
+  (->> s
+       (clojure.string/split-lines)
+       (mapv #(vector (nth % 5) (nth % 36)))))
+
+(def steps (parse (slurp input-file)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println steps))

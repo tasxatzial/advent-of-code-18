cf7e5120b9644fb83d71402402bbf16a9ef0ae4a
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-10-31 04:45:35 +0200

    day8: parse input

--- day08/src/day08/core.clj
+++ day08/src/day08/core.clj
@@ -1,7 +1,17 @@
 (ns day08.core
   (:gen-class))
 
+(def input-file "resources\\input.txt")
+
+(defn parse
+  "Parses the input string and converts it to a vector of numbers."
+  [s]
+  (let [tree-string (first (clojure.string/split-lines s))]
+    (->> (clojure.string/split tree-string #" ")
+         (mapv #(Integer/parseInt %)))))
+
+(def tree (parse (slurp input-file)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println tree))

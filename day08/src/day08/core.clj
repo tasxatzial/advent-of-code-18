412a1bfb6baec63dc3a9019df43bb52cef2cc981
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-11-01 03:31:30 +0200

    day8 problem 1: compute solution

--- day08/src/day08/core.clj
+++ day08/src/day08/core.clj
@@ -1,6 +1,9 @@
 (ns day08.core
   (:gen-class))
 
+; --------------------------
+; common
+
 (def input-file "resources\\input.txt")
 
 (defn parse
@@ -12,6 +15,32 @@
 
 (def tree (parse (slurp input-file)))
 
+; --------------------------
+; problem 1
+
+(defn metadata-sum
+  "Computes the sum of all metadata."
+  ([]
+   (metadata-sum 0))
+  ([header-index]
+   (let [child-count (get tree header-index)]
+     (loop [sum 0
+            i header-index
+            child-count child-count]
+       (if (zero? child-count)
+         (let [meta-count (get tree (inc header-index))
+               meta-sum (apply + (subvec tree (+ i 2) (+ (+ i 2) meta-count)))]
+           [(+ sum meta-sum) (+ i meta-count)])
+         (let [[new-sum new-index] (metadata-sum (+ i 2))]
+           (recur (+ sum new-sum) new-index (dec child-count))))))))
+
+; --------------------------
+; results
+
+(defn day08-1
+  []
+  (metadata-sum))
+
 (defn -main
   []
-  (println tree))
+  (println (day08-1)))

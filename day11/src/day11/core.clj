985900ed00ae01d43dbcb4ab983a54af2b426d1a
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-11-25 18:06:56 +0200

    day11 problem 1: find the cell power

--- day11/src/day11/core.clj
+++ day11/src/day11/core.clj
@@ -1,7 +1,17 @@
 (ns day11.core
   (:gen-class))
 
+(def grid-serial 6392)
+
+(defn find-cell-power
+  "Calculates the power of the [x y] cell."
+  [[x y]]
+  (let [rack-id (+ x 10)
+        power (* rack-id (+ (* y rack-id) grid-serial))]
+    (if (>= power 100)
+      (- (int (/ power 100)) (* 10 (int (/ power 1000))) 5)
+      -5)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println (find-cell-power [3 5])))

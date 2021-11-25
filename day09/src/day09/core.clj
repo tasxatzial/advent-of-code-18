c1c6dcf1fdae6fdd7254e4be23de0066cae0ec3f
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-11-25 02:12:59 +0200

    day9 problem 1: problem 1 refactoring for speed

--- day09/src/day09/core.clj
+++ day09/src/day09/core.clj
@@ -2,106 +2,76 @@
   (:gen-class))
 
 (def players 432)
-(def last-marble 71019)
-
-; --------------------------
-; common
-
-(defn vec-remove
-  "Removes the element in position pos of a vector coll."
-  [pos coll]
-  (into (subvec coll 0 pos) (subvec coll (inc pos))))
-
-(defn vec-add
-  "Adds the element in position pos of a vector coll.
-  If pos == (count coll) the element is added to the end of the vector."
-  [pos coll elem]
-  (into (conj (subvec coll 0 pos) elem) (subvec coll pos)))
 
 ; --------------------------
 ; problem 1
 
-(defn find-add-pos
-  "Returns the position of the marble that will be added next when the
-  previous marble was added in position pos."
-  [pos marble-count]
-  (if (<= (+ pos 2) marble-count)
-    (+ pos 2)
-    (- (+ pos 2) marble-count)))
+(defn marble-positions-after-del
+  "Removes the appropriate marble from the positions vector."
+  [marble-positions]
+  (let [remove-pos (- (count marble-positions) 8)
+        second-part (subvec marble-positions (+ remove-pos 2))
+        first-part (subvec marble-positions 0 remove-pos)
+        curr-marble (get marble-positions (inc remove-pos))]
+    (conj (into second-part first-part) curr-marble)))
 
-(defn find-remove-pos
-  "Returns the position of the marble that will be removed when the
-  previous marble was removed from position pos."
-  [pos marble-count]
-  (if (>= (- pos 7) 0)
-    (- pos 7)
-    (+ marble-count (- pos 7))))
-
-(defn find-pos-after-removed-marble
-  "Finds the position of the current marble after a marble has been
-  removed from position pos."
-  [total-marbles pos]
-  (if (= (dec total-marbles) pos)
-    0
-    pos))
+(defn marble-positions-after-add
+  "Adds a marble to the appropriate position in the positions vector."
+  [marble-positions marble]
+  (conj (subvec marble-positions 1) (first marble-positions) marble))
 
 (defn add-marble
-  "Inserts a marble in the positions vector at position pos and returns a vector
-  with 3 elements:
+  "Inserts a marble to the positions vector and returns a vector with 2 elements:
   1) The updated player scores.
-  2) The position of the next current marble.
-  3) The updated vector of marbles."
-  [pos marble scores positions]
-  (let [add-pos (find-add-pos pos (count positions))
-        new-positions (vec-add add-pos positions marble)]
-    [scores add-pos new-positions]))
+  2) The updated vector of marbles."
+  [marble scores marble-positions]
+  (let [new-positions (marble-positions-after-add marble-positions marble)]
+    [scores new-positions]))
 
 (defn remove-marble
-  "Removes a marble from position pos of the positions vector and returns a vector
-  with 3 elements:
+  "Removes a marble from the positions vector and returns a vector with 2 elements:
   1) The updated player scores.
-  2) The position of the next current marble.
-  3) The updated vector of marbles."
-  [pos marble scores positions player]
-  (let [total-marbles (count positions)
-        removed-pos (find-remove-pos pos total-marbles)
-        new-pos (find-pos-after-removed-marble total-marbles removed-pos)
-        removed-marble (get positions removed-pos)
-        new-positions (vec-remove removed-pos positions)
+  2) The updated vector of marbles."
+  [marble scores marble-positions player]
+  (let [removed-pos (- (count marble-positions) 8)
+        removed-marble (get marble-positions removed-pos)
+        new-positions (marble-positions-after-del marble-positions)
         player-score (get scores player 0)
         new-player-score (+ player-score removed-marble marble)
         new-scores (assoc scores player new-player-score)]
-    [new-scores new-pos new-positions]))
+    [new-scores new-positions]))
 
 (defn make-move
-  "Makes one move in the game and returns the update map of scores."
-  [pos marble scores positions player]
+  "Returns the updated map of scores after a marble has been played."
+  [marble scores marble-positions player]
   (if (zero? (mod marble 23))
-    (remove-marble pos marble scores positions player)
-    (add-marble pos marble scores positions)))
+    (remove-marble marble scores marble-positions player)
+    (add-marble marble scores marble-positions)))
 
 (defn calc-scores
-  "Calculate a map of player scores. Keys run from 0 to (players -1)."
-  []
-  (loop [pos 0
-         marble 1
-         scores {}
-         player 0
-         positions [0]]
-    (if (= marble last-marble)
-      scores
-      (let [[new-scores new-pos new-positions] (make-move pos marble scores positions player)
-            next-player (mod (inc player) players)
-            next-marble (inc marble)]
-        (recur new-pos next-marble new-scores next-player new-positions)))))
+  "Calculate the final map of player scores after the game has ended.
+  Keys run from 0 to (players -1)."
+  [last-marble]
+  (if (= 1 last-marble)
+    {}
+    (loop [marble 2
+           scores {}
+           player 1
+           positions (conj [] 0 1)]
+      (if (= marble last-marble)
+        scores
+        (let [[new-scores new-positions] (make-move marble scores positions player)
+              next-player (mod (inc player) players)
+              next-marble (inc marble)]
+          (recur next-marble new-scores next-player new-positions))))))
 
 ; --------------------------
 ; results
 
-(defn day09-1a
+(defn day09-1
   []
-  (apply max (map second (calc-scores))))
+  (apply max (map second (calc-scores 71019))))
 
 (defn -main
   []
-  (println (day09-1a)))
+  (println (day09-1)))

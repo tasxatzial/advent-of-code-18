f7fbe3198656321a4318bac809e3854943aa1d60
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-11-26 07:38:01 +0200

    day11 problem 1: generalize problem 1 solution for a subgrid of any size

--- day11/src/day11/core.clj
+++ day11/src/day11/core.clj
@@ -3,6 +3,9 @@
 
 (def grid-serial 6392)
 
+; --------------------------
+; problem 1
+
 (defn find-cell-power
   "Calculates the power of the [x y] cell."
   [[x y]]
@@ -13,30 +16,38 @@
       -5)))
 
 (defn create-grid
-  "Creates a vector that contains all [x y] coordinates
-   with 1<=x<=max, 1<=y<=max"
+  "Creates a vector that contains all [x y] coordinates with 1<=x<=max, 1<=y<=max"
   [max]
   (vec (for [x (range 1 (inc max))
              y (range 1 (inc max))]
-         [y x])) )
+         [y x])))
 
-(def subgrid-offsets [0 1 2 300 301 302 600 601 602])
+(defn find-subgrid-offsets
+  "Returns a vector with the relative cell offsets to the top-left cell of a subgrid.
+  The offsets indicate positions in the 300x300 grid."
+  [subgrid-size]
+  (let [x-offsets (range subgrid-size)
+        y-offsets (range 0 (* 300 subgrid-size) 300)]
+    (reduce (fn [result offset]
+              (into result (map #(+ offset %) x-offsets)))
+            [] y-offsets)))
 
 (defn find-subgrid-power
-  "Finds the total power of a 3x3 subgrid."
-  [cells-power topleft-index]
-  (let [subgrid-powers (map #(get cells-power (+ topleft-index %)) subgrid-offsets)]
+  "Finds the total power of a subgrid."
+  [cells-power topleft-index subgrid-size]
+  (let [subgrid-offsets (find-subgrid-offsets subgrid-size)
+        subgrid-powers (map #(get cells-power (+ topleft-index %)) subgrid-offsets)]
     (apply + subgrid-powers)))
 
 (defn find-max-power
-  "Finds the 3x3 grid that has the most power. Returns a vector
-  containing the power and the top-left [x y] coordinates of the grid."
-  []
+  "Finds the subgrid of the given size that has the most power. Returns a vector
+  containing the power and the top-left [x y] coordinate of the subgrid."
+  [subgrid-size]
   (let [cells-power (mapv find-cell-power (create-grid 300))
-        scan-grid (create-grid 298)]
+        scan-grid (create-grid (- 300 (dec subgrid-size)))]
     (reduce (fn [result [x y :as cell]]
               (let [subgrid-index (+ (dec x) (* (dec y) 300))
-                    subgrid-power (find-subgrid-power cells-power subgrid-index)
+                    subgrid-power (find-subgrid-power cells-power subgrid-index subgrid-size)
                     max-power (first result)]
                 (if (> subgrid-power max-power)
                   [subgrid-power cell]
@@ -44,11 +55,15 @@
             [0 0] scan-grid)))
 
 ; --------------------------
+; problem 2
+
+
+; --------------------------
 ; results
 
 (defn day11-1
   []
-  (second (find-max-power)))
+  (second (find-max-power 3)))
 
 (defn -main
   []

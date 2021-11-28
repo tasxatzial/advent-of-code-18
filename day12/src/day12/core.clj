2aced1e5b4c68c8c8dcde519a233a15eddfd4f30
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-11-28 06:10:01 +0200

    day12: parse input

--- day12/src/day12/core.clj
+++ day12/src/day12/core.clj
@@ -1,7 +1,20 @@
 (ns day12.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+(def init-state "##.#.####..#####..#.....##....#.#######..#.#...........#......##...##.#...####..##.#..##.....#..####")
+
+(defn parse-rules
+  [s]
+  (let [data (clojure.string/split-lines s)
+        rules (drop 2 data)]
+    (into {} (map #(clojure.string/split % #" => ") rules))))
+
+(def rules (parse-rules (slurp input-file)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println rules))

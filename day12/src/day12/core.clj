d1885e33aefbc725c7e1ae4ca3d772030299413a
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-11-28 06:48:20 +0200

    day12: change match pattern to a vector of chars and the next state to a char

--- day12/src/day12/core.clj
+++ day12/src/day12/core.clj
@@ -7,11 +7,22 @@
 (def input-file "resources\\input.txt")
 (def init-state "##.#.####..#####..#.....##....#.#######..#.#...........#......##...##.#...####..##.#..##.....#..####")
 
+(defn parse-rule
+  "Parses an input line that represents a rule to the appropriate structure."
+  [rule]
+  (let [split-line (clojure.string/split rule #" => ")
+        match-pattern (into [] (first split-line))
+        next-state (first (seq (second split-line)))]
+    [match-pattern next-state]))
+
 (defn parse-rules
+  "Parses the input string to the appropriate data structure (a map
+  of rules). Each rule has as key a vector that describes the match pattern
+  and as value a char that indicates the next state."
   [s]
   (let [data (clojure.string/split-lines s)
         rules (drop 2 data)]
-    (into {} (map #(clojure.string/split % #" => ") rules))))
+    (into {} (map parse-rule rules))))
 
 (def rules (parse-rules (slurp input-file)))
 

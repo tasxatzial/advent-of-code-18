09e29497b53ee1d9c7319baa9695ead22dbd4a09
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-12-03 20:31:53 +0200

    day13: create the initial tracks structure

--- day13/src/day13/core.clj
+++ day13/src/day13/core.clj
@@ -33,7 +33,53 @@
         parsed (map #(parse-line %1 %2) (range (count split-lines)) split-lines)]
     (reduce into {} parsed)))
 
-(def tracks (parse (slurp input-file)))
+(def input-tracks (parse (slurp input-file)))
+
+(defn get-neighbors
+  "Finds the neighbors of a track located at [x y].
+  Returns a 4 item vector of [[x y] track-type]. Neighbors are listed in the
+  following order: top bottom left right."
+  [[x y] tracks]
+  (let [top (get tracks [x (dec y)])
+        bottom (get tracks [x (inc y)])
+        left (get tracks [(dec x) y])
+        right (get tracks [(inc x) y])]
+    [top bottom left right]))
+
+(defn get-track-by-neighbors
+  "Returns the track type at [x y] by examining its neighbors only.
+  Returns a [[x y] track-type]."
+  [[loc _] tracks]
+  (let [[top bottom left right] (get-neighbors loc tracks)]
+    (cond
+      (and (or (= :vertical top) (= :intersect top))
+           (or (= :horizontal left) (= :intersect left))
+           (or (= :vertical bottom) (= :intersect bottom))
+           (or (= :horizontal right) (= :intersect right))) [loc :intersect]
+      (and (or (= :vertical top) (= :intersect top))
+           (or (= :vertical bottom) (:intersect bottom))) [loc :vertical]
+      (and (or (= :horizontal left) (= :intersect left))
+           (or (= :horizontal right) (= :intersect right))) [loc :horizontal]
+      (and (= :horizontal left) (= :vertical top)) [loc :slash]
+      (and (= :horizontal right) (= :vertical bottom)) [loc :slash]
+      (and (= :horizontal left) (= :vertical bottom)) [loc :backslash]
+      (and (= :horizontal right) (= :vertical bottom)) [loc :backslash])))
+
+(defn find-carts
+  "Finds the tracks that are occupied by carts.
+  Returns a seq of [[x y] :cart]"
+  [tracks]
+  (filter #(= :cart (second %)) tracks))
+
+(defn replace-carts
+  "Scans the initial parsed tracks and replaces all tracks that have a cart
+  character with the correct track character."
+  [tracks]
+  (let [carts (find-carts tracks)
+        replaced (map #(get-track-by-neighbors % tracks) carts)]
+    (into tracks replaced)))
+
+(def tracks (replace-carts input-tracks))
 
 (defn -main
   []

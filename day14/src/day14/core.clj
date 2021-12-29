a95699ef1bde517f32c3c6c020e0064c632554b7
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-12-29 20:35:53 +0200

    day14: calculate the new recipes and positions after one round

--- day14/src/day14/core.clj
+++ day14/src/day14/core.clj
@@ -1,6 +1,30 @@
 (ns day14.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input 681901)
+(def input-vec [6 8 1 9 0 1])
+(def input-count (count input-vec))
+(def starting-recipes [3 7])
+(def elf1-pos 0)
+(def elf2-pos 1)
+
+(defn next-round
+  "Accepts a vector of the already computed recipes and the positions of the
+  elves and returns the new recipes and positions after one round."
+  [recipes elf1-pos elf2-pos]
+  (let [elf1-recipe (get recipes elf1-pos)
+        elf2-recipe (get recipes elf2-pos)
+        recipes-sum (+ elf1-recipe elf2-recipe)
+        new-recipes (if (>= recipes-sum 10)
+                      (conj recipes 1 (- recipes-sum 10))
+                      (conj recipes recipes-sum))
+        new-elf1-pos (rem (+ elf1-pos elf1-recipe 1) (count new-recipes))
+        new-elf2-pos (rem (+ elf2-pos elf2-recipe 1) (count new-recipes))]
+    [new-recipes new-elf1-pos new-elf2-pos]))
+
 (defn -main
   []
-  (println 1))
+  (println (next-round starting-recipes elf1-pos elf2-pos)))

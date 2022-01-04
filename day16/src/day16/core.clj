16d3c40bf1dce3eb8fe3cd42baf095d645f2a6c4
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-01-05 00:10:57 +0200

    day16 problem 1: parse input

--- day16/src/day16/core.clj
+++ day16/src/day16/core.clj
@@ -1,7 +1,31 @@
 (ns day16.core
   (:gen-class))
 
+(def input1 "resources\\samples.txt")
+
+(defn parse-sample
+  "Parses a collection of strings that represent a [before, instruction, after]
+  sequence in the samples file and returns the sequence represented by a vector of
+  3 vectors, each vector contains 4 numbers."
+  [sample]
+  (let [before (second (clojure.string/split (first sample) #": "))
+        instr (str \[ (second sample) \])
+        after (second (clojure.string/split (nth sample 2) #": "))]
+    (mapv read-string [before instr after])))
+
+(defn parse
+  "Parses the input string and returns a collection of samples.
+  Each sample is a vector representing a [before, instruction, after] sequence
+  in the samples file."
+  [s]
+  (->> s
+       (clojure.string/split-lines)
+       (filter #(not= "" %))
+       (partition 3)
+       (map parse-sample)))
+
+(def samples (parse (slurp input1)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println samples))

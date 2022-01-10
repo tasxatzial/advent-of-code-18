fd5590e314c1448d9327de7a5a331e87dd58bcf2
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-01-10 16:16:36 +0200

    day18: parse input

--- day18/src/day18/core.clj
+++ day18/src/day18/core.clj
@@ -1,7 +1,21 @@
 (ns day18.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse
+  "Parses the input string and returns the lumber area represented as a vector of vectors.
+  Each vector represents a line in the input file.
+  The contents of [i j] can be retrieved by get-in."
+  [s]
+  (let [split-lines (clojure.string/split-lines s)]
+    (mapv vec split-lines)))
+
+(def acres (parse (slurp input-file)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println acres))

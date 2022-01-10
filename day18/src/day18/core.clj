1bfe45fcd589c5abc62601aa62374200a0b400a9
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-01-10 16:35:04 +0200

    day18: get the adjacent values of a location

--- day18/src/day18/core.clj
+++ day18/src/day18/core.clj
@@ -16,6 +16,20 @@
 
 (def acres (parse (slurp input-file)))
 
+(defn get-adjacent
+  "Returns the values of adjacent locations to [x y]."
+  [acres [x y]]
+  (let [top-left (get-in acres [(dec x) (dec y)])
+        top (get-in acres [(dec x) y])
+        top-right (get-in acres [(dec x) (inc y)])
+        left (get-in acres [x (dec y)])
+        right (get-in acres [x (inc y)])
+        bottom-left (get-in acres [(inc x) (dec y)])
+        bottom (get-in acres [(inc x) y])
+        bottom-right (get-in acres [(inc x) (inc y)])]
+    (filter (complement nil?)
+            [top-left top top-right left right bottom-left bottom bottom-right])))
+
 (defn -main
   []
-  (println acres))
+  (println (get-adjacent acres [0 0])))

ae8348237d5ce85764b1a4b97754c7529b353010
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-01-11 03:38:52 +0200

    day22: initial commit

---
+++ day22/README.md
@@ -0,0 +1,3 @@
+# day22
+
+[adventofcode.com/2018/day/22](https://adventofcode.com/2018/day/22)
---
+++ day22/src/day22/core.clj
@@ -0,0 +1,15 @@
+(ns day22.core
+  (:gen-class))
+
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(def depth 7863)
+(def target-loc [14 760])
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))

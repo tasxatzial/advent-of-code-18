2bc2eee5ec2b1b540e4ae834a578a21ebcb0d168
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-01-12 01:37:43 +0200

    day22: find the erosion level and geologic index of a location

--- day22/src/day22/core.clj
+++ day22/src/day22/core.clj
@@ -9,7 +9,30 @@
 (def depth 7863)
 (def target-loc [14 760])
 
+(declare memoized-find-geologic-index)
+(declare memoized-find-erosion-level)
+
+(defn find-geologic-index
+  "Finds the geologic index at [x y]."
+  [[x y :as loc]]
+  (cond
+    (and (zero? x) (zero? y)) 0
+    (= loc target-loc) 0
+    (zero? y) (* x 16807)
+    (zero? x) (* y 48271)
+    :else (* (memoized-find-erosion-level [(dec x) y])
+             (memoized-find-erosion-level [x (dec y)]))))
+
+(def memoized-find-geologic-index (memoize find-geologic-index))
+
+(defn find-erosion-level
+  "Finds the erosion level at loc."
+  [loc]
+  (let [geologic-index (memoized-find-geologic-index loc)]
+    (rem (+ geologic-index depth) 20183)))
+
+(def memoized-find-erosion-level (memoize find-erosion-level))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println (find-geologic-index [10 10])))

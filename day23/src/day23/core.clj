ff594cd6c2c4c9f711b1c34bc64c1f9ee69d0f53
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-01-13 23:37:12 +0200

    day23: parse input

--- day23/src/day23/core.clj
+++ day23/src/day23/core.clj
@@ -1,7 +1,29 @@
 (ns day23.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse-line
+  "Parses an input line (string) and returns a data structure that represents
+  a nanobot: a vector of two items, position (vector) & radius."
+  [s]
+  (let [split-line (clojure.string/split s #"pos=<|>, r=")
+        pos (read-string (str \[ (second split-line) \]))
+        radius (Integer/parseInt (get split-line 2))]
+    [pos radius]))
+
+(defn parse
+  "Parses the input string and returns a collection of nanobots.
+  A nanobot is represented by the data structure returned by parse-line."
+  [s]
+  (let [split-lines (clojure.string/split-lines s)]
+    (map parse-line split-lines)))
+
+(def nanobots (parse (slurp input-file)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println nanobots))

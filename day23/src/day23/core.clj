79985b6c7d561abc2fe1c3277a463b0f4be8b410
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-01-14 04:48:32 +0200

    day23 problem 1: compute solution

--- day23/src/day23/core.clj
+++ day23/src/day23/core.clj
@@ -24,6 +24,34 @@
 
 (def nanobots (parse (slurp input-file)))
 
+; --------------------------
+; problem 1
+
+(defn distance
+  "Returns the manhattan distance between pos1 and pos2."
+  [pos1 pos2]
+  (->> (map #(- %1 %2) pos1 pos2)
+       (map #(Math/abs %))
+       (apply +)))
+
+(defn count-within-strongest
+  "Counts how many nanobots are within the range of the strongest nanobot."
+  []
+  (let [max-radius (apply max (map second nanobots))
+        [strongest-pos strongest-radius] (some #(when (= max-radius (second %)) %) nanobots)]
+    (->> nanobots
+         (map first)
+         (map #(distance strongest-pos %))
+         (filter #(<= % strongest-radius))
+         count)))
+
+; --------------------------
+; results
+
+(defn day23-1
+  []
+  (count-within-strongest))
+
 (defn -main
   []
-  (println nanobots))
+  (println (day23-1)))

cdbd65f5c2e6b4b4bfbfeecfddda4a779e5a64b0
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-01-15 07:29:50 +0200

    day25: parse input

--- day25/src/day25/core.clj
+++ day25/src/day25/core.clj
@@ -1,7 +1,19 @@
 (ns day25.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse
+  [s]
+  (->> s
+       clojure.string/split-lines
+       (mapv #(read-string (str \[ % \])))))
+
+(def points (parse (slurp input-file)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println points))

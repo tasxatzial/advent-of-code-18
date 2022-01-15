f4124dee60591133c456c8e7ebbb9f3049f77932
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-01-15 07:36:25 +0200

    day25: find the distances between points

--- day25/src/day25/core.clj
+++ day25/src/day25/core.clj
@@ -14,6 +14,23 @@
 
 (def points (parse (slurp input-file)))
 
+(defn distance
+  "Returns the manhattan distance between p1 and p2."
+  [p1 p2]
+  (->> (map #(- %1 %2) p1 p2)
+       (map #(Math/abs ^int %))
+       (apply +)))
+
+(defn find-distances
+  "Returns a map of distances between a points and all points
+  {i-th point -> distances to all points}."
+  []
+  (reduce (fn [result i]
+            (let [point (get points i)
+                  distances (map #(distance point %) points)]
+              (assoc result i distances)))
+          {} (range (count points))))
+
 (defn -main
   []
-  (println points))
+  (println (find-distances)))
